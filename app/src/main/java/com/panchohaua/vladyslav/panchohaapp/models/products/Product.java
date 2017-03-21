47fbce9e67c9a3f7a349b97fa76a9267127abacd
Vladyslav Tychyna <tychyna.te@gmail.com>
2017-03-21 17:32:58 +0200

    init

---
+++ app/src/main/java/com/panchohaua/vladyslav/panchohaapp/models/products/Product.java
@@ -0,0 +1,12 @@
+package com.panchohaua.vladyslav.panchohaapp.models.products;
+
+/**
+ * Created by Vladyslav on 21.03.2017.
+ */
+
+public class Product {
+    public int id;
+    public String name;
+    public String image;
+    public String price;
+}
